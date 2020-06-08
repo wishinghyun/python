a2b06ce24ad1caf455136179c479c0bce3c36f17
KimHyunSoo <wishinghyun@naver.com>
2020-06-08 16:23:51 +0900

    유튜브 url로 검색하기

--- grammar/11_search_youtube.py
+++ grammar/11_search_youtube.py
@@ -6,14 +6,14 @@ search = input("검색어를 입력해 주세요 : ")
 q = f'q={search}'
 my_type = 'type=video'
 part = 'part=snippet'
-maxResults = 'maxResults=50'
+maxResults = 'maxResults=20'
 
-url = f'https://www.googleapis.com/youtube/v3/search?key={key}&{part}&{my_type}%{q}'
+url = f'https://www.googleapis.com/youtube/v3/search?key={key}&{part}&{my_type}%{q}&{maxResults}'
 
 response = requests.get(url)
 datas = response.json()
 
 # 채널명, 영상 제목
-for data in datas['items']:
+for data in datas['items'][:10]:
     print(data['snippet']['title'], end=' /채널명 ') # end 붙이면 2개의 print가 한 줄로 출력됨
     print(data['snippet']['channelTitle'])
